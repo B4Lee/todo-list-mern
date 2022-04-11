3a8dbec08f26fc911a4a08ccb0219f203896c268
B4Le <iqbalanggoro48@gmail.com>
2022-04-11 15:47:01 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const PORT = 8080;
-const uri = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 8080;
+// const uri = process.env.MONGODB_URI;
 
 const app = express();
 const todoRoutes = require("./routes/todoRoutes")
@@ -15,7 +15,7 @@ todoRoutes. */
 app.use(express.json())
 app.use(cors());
 
-mongoose.connect(uri, connectionOptions)
+mongoose.connect("mongodb://localhost/todolist", connectionOptions)
     .then(() => console.log("connected successfully"))
     .catch((err) => console.error(err))
 
